6da49fa3220b672381862148bbe42e2ef22bcf3f
Utsav Karan <ukaran11@gmail.com>
2021-12-04 17:59:17 +0530

    Utsav | Basic Sidebar icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { LineStyle, Timeline, TrendingUp } from '@material-ui/icons';
+
 import './sidebar.css';
+
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            sidebar
+          <div className="sidebarWrapper">
+              <div className="sidebarMenu">
+                  <h3 className="sidebarTitle">Dashboard</h3>
+                  <ul className="sidebarList">
+                      <li className="sidebarListItem">
+                        <LineStyle />
+                        Home
+                      </li>
+                      <li className="sidebarListItem">
+                        <Timeline />
+                        Analytics
+                      </li>
+                      <li className="sidebarListItem">
+                        <TrendingUp />
+                        Sales
+                      </li>
+                  </ul>
+              </div> 
+          </div>
         </div>
     )
 }

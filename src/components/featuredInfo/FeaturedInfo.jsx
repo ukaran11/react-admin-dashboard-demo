ff9944e6ba842b12a842f17f225b496650e660f8
Utsav Karan <ukaran11@gmail.com>
2021-12-07 23:20:12 +0530

    Utsav | Created FeaturedInfo component

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./featuredInfo.css";
+
+const FeaturedInfo = () => {
+    return (
+        <div className="featured">
+            <div className="featuredItem"></div>
+        </div>
+    )
+}
+
+export default FeaturedInfo

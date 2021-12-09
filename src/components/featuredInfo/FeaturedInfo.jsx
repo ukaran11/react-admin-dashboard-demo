0149e8ef881c1c11343a00235a4fe9c1addc7a1f
Utsav Karan <ukaran11@gmail.com>
2021-12-09 18:18:06 +0530

    Utsav | Worked on FeaturedInfo

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./featuredInfo.css";
-
+import { ArrowDownward } from '@material-ui/icons';
 const FeaturedInfo = () => {
     return (
         <div className="featured">
@@ -8,8 +8,11 @@ const FeaturedInfo = () => {
                 <span className="featuredTitle">Revenue</span>
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$2,415</span>
-                    <span className="featuredMoneyRate">-11.4</span>
+                    <span className="featuredMoneyRate">
+                        -11.4 <ArrowDownward />
+                    </span>
                 </div>
+                <span className="featuredSub">Compared to Last Month</span>
             </div>
         </div>
     )

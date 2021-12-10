42ab694ed2ba39cf789b36d6794c38ef80f2cf05
Utsav Karan <ukaran11@gmail.com>
2021-12-10 22:26:14 +0530

    Utsav | Included FeaturedInfo in Home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import FeaturedInfo from '../../featuredInfo/FeaturedInfo';
 import "./Home.css";
 const Home = () => {
     return (
         <div className="home">
-            Home
+           <FeaturedInfo />
         </div>
     )
 }

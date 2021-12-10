9d9551bbf7e0e6566d405595b2ffc184c7b0d20c
Utsav Karan <ukaran11@gmail.com>
2021-12-11 00:11:39 +0530

    Utsav | Added more featuredInfo styling

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -9,7 +9,7 @@ const FeaturedInfo = () => {
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$2,415</span>
                     <span className="featuredMoneyRate">
-                        -11.4 <ArrowDownward />
+                        -11.4 <ArrowDownward className="featuredIcon negative"/>
                     </span>
                 </div>
                 <span className="featuredSub">Compared to Last Month</span>
@@ -19,7 +19,7 @@ const FeaturedInfo = () => {
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$4,415</span>
                     <span className="featuredMoneyRate">
-                        -1.4 <ArrowDownward />
+                        -1.4 <ArrowDownward className="featuredIcon negative"/>
                     </span>
                 </div>
                 <span className="featuredSub">Compared to Last Month</span>
@@ -29,7 +29,7 @@ const FeaturedInfo = () => {
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$2,255</span>
                     <span className="featuredMoneyRate">
-                        +2.4 <ArrowUpward />
+                        +2.4 <ArrowUpward className="featuredIcon"/>
                     </span>
                 </div>
                 <span className="featuredSub">Compared to Last Month</span>

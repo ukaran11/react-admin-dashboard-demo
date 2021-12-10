e529c4a97340fb2f7e860b9bf5738c51df26686d
Utsav Karan <ukaran11@gmail.com>
2021-12-10 22:54:34 +0530

    Utsav | Added more items in featuredInfo

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import "./featuredInfo.css";
-import { ArrowDownward } from '@material-ui/icons';
+import "./FeaturedInfo.css";
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons';
 const FeaturedInfo = () => {
     return (
         <div className="featured">
@@ -10,6 +10,26 @@ const FeaturedInfo = () => {
                     <span className="featuredMoney">$2,415</span>
                     <span className="featuredMoneyRate">
                         -11.4 <ArrowDownward />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to Last Month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Sales</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$4,415</span>
+                    <span className="featuredMoneyRate">
+                        -1.4 <ArrowDownward />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to Last Month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Cost</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,255</span>
+                    <span className="featuredMoneyRate">
+                        +2.4 <ArrowUpward />
                     </span>
                 </div>
                 <span className="featuredSub">Compared to Last Month</span>

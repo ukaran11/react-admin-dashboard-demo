3b255a8b3f50dfcc07cbeb961c84188ff961b9bc
Utsav Karan <ukaran11@gmail.com>
2021-12-12 13:44:03 +0530

    Utsav | Added charts component

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Chart.css";
+const Chart = () => {
+    return (
+        <div className="chart">
+            
+        </div>
+    )
+}
+
+export default Chart
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Chart from '../../Chart/Chart';
 import FeaturedInfo from '../../featuredInfo/FeaturedInfo';
 import "./Home.css";
 const Home = () => {
     return (
         <div className="home">
            <FeaturedInfo />
+           <Chart />
         </div>
     )
 }
